c0308bca7007c474075ffa6638da2a08b5433f1c
Matt <hey@mattt.space>
2026-04-09 20:19:47 -0700

    cleanup unused import

--- api/index.mjs
+++ api/index.mjs
@@ -1,5 +1,4 @@
 import { handle } from "../build/dev/javascript/avid/server/router.mjs";
-import { createServer } from "../build/dev/javascript/avid/server/server_ffi.mjs";
 
 export default async function handler(req, res) {
   handle(req, res);
